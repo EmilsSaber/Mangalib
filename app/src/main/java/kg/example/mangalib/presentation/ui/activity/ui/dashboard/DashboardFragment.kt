e26332d9815b791b7436a008c82434a73a981242
Emil <emil.mametov.01@gmail.com>
2023-01-24 22:10:56 +0600

    recycler_review and others

--- app/src/main/java/kg/example/mangalib/presentation/ui/activity/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/kg/example/mangalib/presentation/ui/activity/ui/dashboard/DashboardFragment.kt
@@ -9,35 +9,15 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import kg.example.mangalib.databinding.FragmentDashboardBinding
 
-
 class DashboardFragment : Fragment() {
-
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        binding = FragmentDashboardBinding.inflate(layoutInflater)
+        return binding.root
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+}}
